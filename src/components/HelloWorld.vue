fd6bfb490d75c0f96d7b0f3a6f506798cd6c8079
denghong <991331650@qq.com>
2025-08-01 14:16:27 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <button @click="readIcCard">readIcCard读卡</button>
     <div class="order-item" v-for="item in 6" :key="item">
       <!-- 订单顶部 -->
       <div class="order-header">
@@ -18,9 +19,7 @@
           <div class="item-name">
             世嘉 VOCALOID 初音未来 雪未来 SNOW MIKU 2012 毛绒 再版 哔哩哔哩独家
           </div>
-          <div class="item-skus">
-            雪未来 毛绒
-          </div>
+          <div class="item-skus">雪未来 毛绒</div>
         </div>
       </div>
       <!-- 订单总计 -->
@@ -63,37 +62,87 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data() {
     return {
       value: 4.5,
       value2: 60,
       tableData: [
         {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
         },
         {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
         },
         {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
         },
         {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
       ],
-      checked: true
-    }
-  }
-}
+      checked: true,
+    };
+  },
+  methods: {
+    //读卡操作
+    readIcCard() {
+      try {
+        //alert('测试');
+        let iport = "100"; //读卡器端口，读卡器端口，USB1--100,COM1--0,COM2--1，一般默认为100，打开页面时从后台获取
+        let iBaud = "9600"; //波特率，一般默认为9600，打开页面时从后台获取
+        let inpara = iport + "|" + iBaud;
+        //创建 XMLHttpRequest 对象
+        let xhr = new XMLHttpRequest();
+        // let url = 'http://localhost:18080/readIcCard';
+        let url = "http://localhost:9999/users";
+        xhr.open("post", url, true);
+
+        // xhr.setRequestHeader('Content-Type', 'application/text');
+        // xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        // xhr.setRequestHeader('auth', '123');
+        xhr.onreadystatechange = function () {
+          if (
+            xhr.readyState === 4 &&
+            xhr.status === 200 &&
+            xhr.responseText != "9999"
+          ) {
+            console.log(xhr.responseText);
+            //读卡返回信息，可以用该数据验证测试功能
+            //var rcardstr=0|68010027028300000000990101000002000050000000000000000100000100025036680900201007828802D816|0000271000000009|680100FC0000522400005224000052240000522400005224000052240000522400005224000052240000522400005224000052240000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008516|680100FC00005224000052240000522400005224000052240000522400005224000052240000522400005224000052240000522400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+            var rcardstr = xhr.responseText;
+            var rcararr = rcardstr.split("|");
+            var rkcode = rcararr[0];
+            var file1 = rcararr[1]; //读卡成功后发送后台，参数信息文件
+            var file2 = rcararr[2]; //读卡成功后发送后台，钱包文件
+            var file3 = rcararr[3]; //读卡成功后发送后台，第一套电价文件
+            var file4 = rcararr[4]; //读卡成功后发送后台，第二套电价文件
+            var file5 = rcararr[5]; //读卡成功后发送后台，电表返写信息文件
+            console.log(file1);
+            console.log(file2);
+            console.log(file3);
+            console.log(file4);
+            console.log(file5);
+            /*调用后端服务，将5个文件（字符串）发送给后台解析，并返回用户信息、写卡信息和购电记录*/
+          }
+        };
+        //为准备发送做准备将要发送的数据作为参数传递给 send() 方法。
+        //处理响应当 xhr.readyState === 4 并且 xhr.status === 200 时，请求已成功。使用 xhr.responseText 获取响应数据。
+        xhr.send(inpara);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -124,7 +173,7 @@ export default {
         display: flex;
         align-items: center;
         &::before {
-          content: ' ';
+          content: " ";
           display: block;
           position: absolute;
           right: -20px;
